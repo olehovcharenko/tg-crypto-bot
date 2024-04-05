79fbb29f06b70f0d5246d0d1fcd91d110f49f2ed
Oleh Ovcharenko <oleh.ovcharenko@arbitas.com>
2024-04-05 14:44:53 +0300

    fix DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,26 @@ import { BotModule } from './telegram/bot.module';
 import { WalletModule } from './wallet/wallet.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { WalletEntity } from './wallet/wallet.entity';
+import { config } from 'dotenv';
 
+config();
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         entities: [WalletEntity],
       }),
-    }), WalletModule, BotModule,
+    }),
+    WalletModule,
+    BotModule,
   ],
   controllers: [AppController],
   providers: [AppService],

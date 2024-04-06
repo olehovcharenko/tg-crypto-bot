280c03350d2a456ef11d465ace43087d4c3d202a
Oleh Ovcharenko <oleh.ovcharenko@arbitas.com>
2024-04-06 11:34:55 +0300

    fix

--- src/telegram/bot.module.ts
+++ src/telegram/bot.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WalletEntity } from './wallet.entity';
-import { TransactionEntity } from './transaction.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WalletEntity, TransactionEntity])],
+  imports: [TypeOrmModule.forFeature([WalletEntity])],
   controllers: [],
   providers: [BotService],
 })
